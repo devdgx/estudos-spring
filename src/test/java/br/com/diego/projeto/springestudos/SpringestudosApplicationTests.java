708617f4d46ac50d75bd06dbd413b1f920b24e11
diego <devdgxx@gmail.com>
2025-10-11 11:51:21 -0400

    Initial commit

---
+++ src/test/java/br/com/diego/projeto/springestudos/SpringestudosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diego.projeto.springestudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringestudosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
