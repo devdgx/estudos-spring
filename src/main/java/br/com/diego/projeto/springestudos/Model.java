52e8bf37d8b7e9aea417f4c88bdcdd57e57eed26
diego <devdgxx@gmail.com>
2025-10-14 00:53:12 -0400

    Adiciona classe Model com @Entity, @Table e atributos name, email e age

---
+++ src/main/java/br/com/diego/projeto/springestudos/Model.java
@@ -0,0 +1,48 @@
+package br.com.diego.projeto.springestudos;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class Model {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
+    private String name;
+    private String email;
+    private int age;
+
+    public Model() {
+    }
+
+    public Model(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
