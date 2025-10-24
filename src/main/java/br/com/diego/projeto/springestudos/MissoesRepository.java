aca9b9392421b3d38837970511dd029c2aaed341
diego <devdgxx@gmail.com>
2025-10-23 23:18:41 -0400

    Adiciona interface MissoesRepository com JpaRepository

---
+++ src/main/java/br/com/diego/projeto/springestudos/MissoesRepository.java
@@ -0,0 +1,6 @@
+package br.com.diego.projeto.springestudos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<Missoes, Long> {
+}
