1f3b7c69ab683904efbc33d058d88daabeb794a4
Diego <devdgxx@gmail.com>
2025-11-10 23:42:06 -0400

    Implementa endpoints de listagem, busca, criação, atualização e deleção

--- src/main/java/br/com/diego/projeto/springestudos/missoes/MissoesController.java
+++ src/main/java/br/com/diego/projeto/springestudos/missoes/MissoesController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("missoes")
+@RequestMapping("/missoes")
 public class MissoesController {
 
     private MissoesService missoesService;
