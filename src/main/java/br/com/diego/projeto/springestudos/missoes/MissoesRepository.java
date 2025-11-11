77a50dc23289f1a6ae5339039fed5c7e02d18eb8
Diego <devdgxx@gmail.com>
2025-11-10 23:45:49 -0400

    feat: cria MissoesRepository para operações com a entidade MissoesModel

--- src/main/java/br/com/diego/projeto/springestudos/missoes/MissoesRepository.java
+++ src/main/java/br/com/diego/projeto/springestudos/missoes/MissoesRepository.java
@@ -1,7 +1,6 @@
 package br.com.diego.projeto.springestudos.missoes;
 
-import br.com.diego.projeto.springestudos.ninja.Missoes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MissoesRepository extends JpaRepository<Missoes, Long> {
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
 }
