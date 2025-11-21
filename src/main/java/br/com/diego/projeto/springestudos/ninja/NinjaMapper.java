24e50cf89ba35bbeae3e9d1aaf5013787401815f
Diego <devdgxx@gmail.com>
2025-11-20 22:11:41 -0400

    Chore: incluir a versão do DTO pela intidade e Mapeia manualmente no NinjaMapper os caminhos entidade x dto

---
+++ src/main/java/br/com/diego/projeto/springestudos/ninja/NinjaMapper.java
@@ -0,0 +1,34 @@
+package br.com.diego.projeto.springestudos.ninja;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class NinjaMapper {
+
+    public NinjaModel map (NinjaDTO ninjaDTO){
+        NinjaModel ninjaModel = new NinjaModel();
+
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setName(ninjaDTO.getName());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setAge(ninjaDTO.getAge());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map (NinjaModel ninjaModel){
+
+        NinjaDTO ninjaDTO = new NinjaDTO();
+
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setName(ninjaModel.getName());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setAge(ninjaModel.getAge());
+        ninjaDTO.setRank(ninjaModel.getRank());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+
+        return ninjaDTO;
+    }
+}
