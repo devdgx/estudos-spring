e6e00ae3f153f9c72a3ca62229d357d734bc923b
Diego <devdgxx@gmail.com>
2025-11-20 22:13:37 -0400

    feat: incluir dto como abstracao para minha intidade na camada de service e controller

---
+++ src/main/java/br/com/diego/projeto/springestudos/missoes/MissoesMapper.java
@@ -0,0 +1,4 @@
+package br.com.diego.projeto.springestudos.missoes;
+
+public class MissoesMapper {
+}
---
+++ src/main/java/br/com/diego/projeto/springestudos/ninja/NinjaDTO.java
@@ -0,0 +1,22 @@
+package br.com.diego.projeto.springestudos.ninja;
+
+import br.com.diego.projeto.springestudos.missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NinjaDTO {
+
+    private long id;
+
+    private String name;
+    private String email;
+    private int age;
+    private String rank;
+    private MissoesModel missoes;
+
+
+}
